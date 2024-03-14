88a86a731fa70a99aee53a041adfb563529cda7d
BiTiS2004 <117881913+sybui2004@users.noreply.github.com>
2024-03-14 15:06:28 +0700

    Update MyButton.java

--- BUOI 12 BAI TAP CUOI KHOA 1 - THU VIEN DO HOA SWING/Swing/src/MyButton.java
+++ BUOI 12 BAI TAP CUOI KHOA 1 - THU VIEN DO HOA SWING/Swing/src/MyButton.java
@@ -2,20 +2,11 @@ import java.awt.*;
 import javax.swing.*;
 
 public class MyButton extends JButton {
+    Font customFont = new Font("Arial", Font.BOLD, 18);
     public MyButton() {
-        setText("Here");
-        setContentAreaFilled(false); // erase area around button
-        setBorder(BorderFactory.createEmptyBorder()); // erase bolder
+        setText("Update Time");
         setFocusable(false);
-        setBounds(50, 100, 100, 50);
+        setFont(customFont);
+        setBackground(Color.PINK);
     }
-
-    @Override
-    public void paintComponent(Graphics graphics) {
-        Graphics2D g2d = (Graphics2D) graphics;
-        g2d.setColor(Color.PINK);
-        g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 50, 50);
-        super.paintComponent(graphics);
-    }
-
 }
